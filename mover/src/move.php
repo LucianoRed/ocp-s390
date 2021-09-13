2cc511054a2708361ae32ce83a98e0d58057cd12
LucianoRed <lscorsin@redhat.com>
2021-09-13 02:49:09 -0300

    Gcloud incluso

--- mover/src/move.php
+++ mover/src/move.php
@@ -1,8 +1,8 @@
 <?php
 $cloud = $_GET['cloud'];
-$namespace = "1629396278450";
+$namespace = "techu-demo";
 if(file_exists("/tmp/cloud.txt")) {
-    $atual = file_get_contents("/tmp/cloud.txt");
+    $atual = chop(file_get_contents("/tmp/cloud.txt"));
 } else {
     $atual = "s390x";
 }
@@ -15,6 +15,10 @@ if($cloud == "s390x") {
     $endereco_api_destino = getenv("ENDERECO_API_S390");
     $token_api_destino = getenv("TOKEN_API_S390");
 }
+if($cloud == "gcloud") {
+    $endereco_api_destino = getenv("ENDERECO_API_GCLOUD");
+    $token_api_destino = getenv("TOKEN_API_GCLOUD");
+}
 if($atual == "aws") {
     $endereco_api_origem = getenv("ENDERECO_API_AWS");
     $token_api_origem = getenv("TOKEN_API_AWS");
@@ -23,9 +27,13 @@ if($atual == "s390x") {
     $endereco_api_origem = getenv("ENDERECO_API_S390");
     $token_api_origem = getenv("TOKEN_API_S390");
 }
+if($atual == "gcloud") {
+    $endereco_api_origem = getenv("ENDERECO_API_GCLOUD");
+    $token_api_origem = getenv("TOKEN_API_GCLOUD");
+}
 $comando =  "echo $cloud > /tmp/cloud.txt";
 exec($comando);
-$comando = "ansible-playbook /playbooks/deploy_app.yaml -e \"ENDERECO_API_DESTINO=$endereco_api_destino TOKEN_API_DESTINO=$token_api_destino ENDERECO_API_ORIGEM=$endereco_api_origem TOKEN_API_ORIGEM=$token_api_origem NAMESPACE=$namespace\" ";
+$comando = "ansible-playbook /playbooks/deploy_app_oc.yaml -e \"ENDERECO_API_DESTINO=$endereco_api_destino TOKEN_API_DESTINO=$token_api_destino ENDERECO_API_ORIGEM=$endereco_api_origem TOKEN_API_ORIGEM=$token_api_origem CLOUD=$cloud NAMESPACE=$namespace\" ";
 echo $comando;
 exec($comando, $Matriz);
 print_r($Matriz);
